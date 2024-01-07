2303ddecdbb8d87bf3dddf7e4aea36a5b487de60
Hamza Rabah <h.rabah@outlook.com>
2024-01-07 16:35:50 +0300

    add api middlewares to handle cors, https, gzip

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 
 from src.app.configurator.config import get_settings
 from src.app.containers import Container
 from src.app.endpoints.root import rootRouter
 from src.app.endpoints.v1.business import businessRouter
+from fastapi.middleware.gzip import GZipMiddleware
 
 
 def create_app() -> FastAPI:
@@ -12,13 +16,22 @@ def create_app() -> FastAPI:
     if not settings.USE_IN_MEMORY_DB:
         db = container.db()
         db.create_database()
-
     fast_api_app = FastAPI(
         title=settings.API_TITLE,
         version=settings.API_VERSION,
         openapi_tags=[{"name": "Business"}],
     )
-    # app.container = container
+    fast_api_app.add_middleware(GZipMiddleware)
+    if settings.APP_ENV == "prod":
+        fast_api_app.add_middleware(HTTPSRedirectMiddleware)
+    fast_api_app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
+    fast_api_app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     fast_api_app.include_router(rootRouter)
     fast_api_app.include_router(businessRouter)
     return fast_api_app
