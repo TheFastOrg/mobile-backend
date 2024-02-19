9630992a709ad1113c0c998fc1e1474e59ad5901
DjObYtE <obaydaba96@gmail.com>
2024-02-19 23:58:45 +0300

    feat: add seed data

---
+++ seed/1_business.sql
@@ -0,0 +1,54 @@
+INSERT INTO
+    business (
+        id,
+        "location",
+        address_line1,
+        address_line2,
+        ar_name,
+        city,
+        country,
+        en_name,
+        slug,
+        status,
+        "type",
+        created_at,
+        updated_at,
+        deleted_at,
+        is_deleted
+    )
+VALUES
+    (
+        '7a1dc9f6-88aa-4442-ae4a-963688446cab',
+        'SRID=4326;POINT (36.28978 33.52096)',
+        'Arnous',
+        'عرنوس',
+        'موز ولوز',
+        'Moz & Loz',
+        'damascus',
+        'syria',
+        'moz-loz',
+        'draft',
+        'restaurant',
+        '2023-10-25 11:57:27.858491+03',
+        NULL,
+        NULL,
+        false
+    ),
+    (
+        'e008b4db-59d8-4fae-9a5b-a3c53a21607b',
+        'SRID=4326;POINT (36.28768 33.50901)',
+        'Al Baramkeh',
+        'البرامكة',
+        'سويف',
+        'Suave',
+        'damascus',
+        'syria',
+        'suave',
+        'draft',
+        'restaurant',
+        '2023-10-25 11:57:27.858491+03',
+        NULL,
+        NULL,
+        false
+    ),
+;
